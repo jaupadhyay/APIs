6610fabb6f001a99bc55d87391874b17bfcf6e3b
jaupadhyay <40603828+jaupadhyay@users.noreply.github.com>
2021-03-08 14:45:49 +0000

    Update show_environment.sh

--- src/show_environment.sh
+++ src/show_environment.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Hello Buildkite Agent $BUILDKITE_AGENT_NAME"
+echo "Hello Buildkite Agent $BUILDKITE_SOURCE"
+if [[ "$BUILDKITE_SOURCE" == "webhook" ]]; then
+echo "Build Source  webhook"
+else
+echo "Build Source WEB"
+fi
