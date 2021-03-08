2b6a9dcaec01ebe8599ea045a401f6ffceaae413
jaupadhyay <40603828+jaupadhyay@users.noreply.github.com>
2021-03-08 15:48:14 +0000

    Update show_environment.sh

--- src/show_environment.sh
+++ src/show_environment.sh
@@ -7,3 +7,19 @@ echo "Build Source  webhook"
 else
 echo "Build Source WEB"
 fi
+
+devchange=$(git log -n 20 --name-only --oneline --pretty="" | src )
+
+echo "Dev changes Agent $devchange"
+
+if [[ -z ${TF_NAME} ]]; then 
+echo "TF_NAME Env Variable not passed"
+TF_NAME="platform"
+echo "TF_NAME Env Variable value set through scrtip value is  $TF_NAME"
+else
+echo "TF_NAME Env Variable passed with value $TF_NAME"
+fi
+
+if [[( -n $devchange )]]; then
+echo "Dev changes Found"
+fi
